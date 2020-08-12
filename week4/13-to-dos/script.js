c87e6e9d898576f149735c5a1acb1e98e90e72ef
Destanee Glenn <destanee.glenn@gmail.com>
2020-08-11 21:12:29 -0500

    Pushing week 5 day 1&2 classwork

--- week4/13-to-dos/script.js
+++ week4/13-to-dos/script.js
@@ -4,3 +4,52 @@ var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+
+renderTodos();
+
+function renderTodos() {
+
+    todoList.innerHTML = "";
+    todoCountSpan.textContent = todos.length;
+
+//Render a new li for each to do 
+for (var i = 0; i < todos.length; i++) {
+    var todo = todos[i];
+
+    var li = document.createElement("li");
+    li.textContent = todo;
+//code here
+
+
+
+    todoList.appendChild(li);
+    }
+}
+
+
+todoForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+
+    var todoText = todoInput.value.trim();
+
+    if(todoText === "") {
+        return;
+    }
+
+    todos.push(todoText);
+    todoInput.value = "";
+
+
+    renderTodos();
+});
+
+
+todoList.addEventListener("click", function(event) {
+    var element = event.target;
+
+    if(element.matches("button") === true) {
+        var index = element.parentElement.getAttribute("data-index");
+        
+    }
+})
